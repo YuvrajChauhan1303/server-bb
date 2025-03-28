28580c77f1f0c76ba32098309bdac7e870f2418d
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2025-03-28 20:09:10 +0530

    fix test 3

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
 const app = express();
-const port = 3000;
-const filePath = 'data.txt';
 
 app.use(express.json());
 
-// Route to receive data from Raspberry Pi and store it
-app.post('/send', (req, res) => {
-    const data = req.body.data;
-    if (!data) {
-        return res.status(400).send('No data received');
-    }
-    fs.writeFile(filePath, data, (err) => {
-        if (err) {
-            return res.status(500).send('Error writing file');
-        }
-        res.send('Data stored successfully');
-    });
+let storedData = ""; // Store data in memory
+
+// Route to receive data
+app.post("/send", (req, res) => {
+  const { data } = req.body;
+  if (!data) {
+    return res.status(400).send("No data received");
+  }
+  storedData = data; // Store data in memory
+  res.send("Data stored successfully");
 });
 
-// Route to send stored data to ESP8266
-app.get('/receive', (req, res) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading file');
-        }
-        res.send(data);
-    });
+// Route to send stored data
+app.get("/receive", (req, res) => {
+  if (!storedData) {
+    return res.status(404).send("No data available");
+  }
+  res.send(storedData);
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
+// Export the app for Vercel
+module.exports = app;
